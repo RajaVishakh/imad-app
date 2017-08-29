418efce56ab4509a2f5701808f761d3a8d7df892
RajaVishakh <31409784+RajaVishakh@users.noreply.github.com>
2017-08-29 19:10:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article.html'));
+  res.sendFile( 'article.html');
 });
 
 app.get('/ui/style.css', function (req, res) {
